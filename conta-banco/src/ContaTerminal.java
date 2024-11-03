0b112c02f42d73182097148f7de3540bb1d33426
NinjaFlor <[200~Repository>
2024-11-03 17:01:46 -0300

    Conclusão do código para o desafio proposto = DIO - Trilha Java Básico. Sintaxe - desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO:Conhecer e importar a classe Scanner
+
+        Scanner scanner = (new Scanner(System.in)).useLocale(Locale.US);
+
+        // Exibir as mensagens para o nosso usuário
+        // Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor,digite o número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor,digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor,digite o nome do cliente: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor,digite o saldo: ");
+        Double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem conta criada
+
+        System.out.println("\n Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " +
+                saldo + " já está disponível para saque");
+
+        scanner.close();
     }
-    
+
 }
